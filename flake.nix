a68bb3d1212fd10f73a9d45e4ddb0bd4fbcd75ce
RocketRace <git@olivialta.cc>
2025-03-23 00:03:46 +0200

    clone

--- flake.nix
+++ flake.nix
@@ -1,34 +1,22 @@
 {
-  description = "my nix server iso";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
 
-  outputs = {
+  outputs = inputs@{
     self,
     nixpkgs,
     nixos-hardware,
-  }: {
-    nixosConfigurations =
-      let base = {
-        system = "x86_64-linux";
-        modules = [
-          # hardware requirements
-          nixos-hardware.nixosModules.apple-macbook-pro-11-1
-          # required for proprietary drivers
-          { nixpkgs.config.allowUnfree = true; }
-        ];
-      }; in {
-        iso = nixpkgs.lib.nixosSystem base;
-        real = nixpkgs.lib.nixosSystem (
-          base // {
-            modules = [
-              # add modules here if necessary
-              {
-                services.tailscale.enable = true;
-              }
-            ];
-          }
-        );
-      };
+    ...
+  }:
+  {
+    nixosConfigurations."caique" = nixpkgs.lib.nixosSystem {
+      system = "x86_84-linux";
+      modules = [
+        nixos-hardware.nixosModules.apple-macbook-pro-11-1
+        ./configuration.nix
+      ];
+    };
   };
 }
