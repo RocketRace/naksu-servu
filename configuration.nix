4944f73a310dd60adfbd125e63c0955e5b4713bf
Olivia <git@olivialta.cc>
2025-03-23 14:36:30 +0200

    Allow a closed lid

--- configuration.nix
+++ configuration.nix
@@ -83,6 +83,9 @@
     };
   };
 
+  # Let me keep the lid closed
+  services.logind.lidSwitch = "ignore";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
