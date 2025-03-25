beceb59c6d259d5de1408c1a587abe835f9bab93
RocketRace <git@olivialta.cc>
2025-03-25 20:21:49 +0200

    Autostart

--- services.nix
+++ services.nix
@@ -2,7 +2,12 @@
 
 {
   systemd.services."oliviabot" = {
+    # Test service
+    description = "I'm automatic olivia!";
     path = [ pkgs.coreutils ];
     script = "${pkgs.coreutils}/bin/echo hi!";
+    # Start automatically on boot after we have network access
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
   };
 }
