233fb558e9725a0ebbd93d8764714ce0dd439df3
RocketRace <git@olivialta.cc>
2025-03-25 20:25:29 +0200

    oliviabot

--- services.nix
+++ services.nix
@@ -3,7 +3,6 @@
 {
   systemd.services."oliviabot" = {
     # Test service
-    description = "I'm automatic olivia!";
     path = [ pkgs.coreutils ];
     script = "${pkgs.coreutils}/bin/echo hi!";
     # Start automatically on boot after we have network access
