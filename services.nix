35a42210caaa4c7c83a2871e7c93ff31faa3f640
RocketRace <git@olivialta.cc>
2025-03-26 18:40:12 +0200

    Don't bother setting up ssh

--- services.nix
+++ services.nix
@@ -5,7 +5,7 @@
     # Start automatically on boot after we have network access
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
-    path = [ pkgs.openssh pkgs.git pkgs.nix ];
+    path = [ pkgs.git pkgs.nix ];
     # Additional setup: scp oliviabot.db, config.py and discord.log into /home and move it to the appropriate folder 
     preStart = ''
       cd /home/olivia/services
@@ -13,7 +13,7 @@
         cd oliviabot
         git pull --rebase
       else
-        git clone git@github.com:RocketRace/oliviabot.git
+        git clone https://github.com:RocketRace/oliviabot.git
       fi
     '';
     script = ''
