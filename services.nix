c6c07fb6afc669f9c7a97ed4eb5c92bfa65ea229
RocketRace <git@olivialta.cc>
2025-03-26 18:13:27 +0200

    clean path

--- services.nix
+++ services.nix
@@ -5,13 +5,14 @@
     # Start automatically on boot after we have network access
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
+    path = [ pkgs.openssh, pkgs.git, pkgs.nix ];
     preStart = ''
       cd /home/olivia/services
       if [ -d oliviabot ]; then
         cd oliviabot
-        ${pkgs.git}/bin/git pull --rebase
+        git pull --rebase
       else
-        ${pkgs.git}/bin/git clone https://github.com/RocketRace/oliviabot.git
+        git clone git@github.com/RocketRace/oliviabot.git
       fi
     '';
     script = ''
