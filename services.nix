f38b146f8810b867ea2799acd0dd2859986a7c51
RocketRace <git@olivialta.cc>
2025-03-26 17:52:20 +0200

    the nixification commences

--- services.nix
+++ services.nix
@@ -2,10 +2,21 @@
 
 {
   systemd.services."oliviabot" = {
-    # Test service
-    script = "echo ${inputs.oliviabot}";
     # Start automatically on boot after we have network access
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
+    preStart = ''
+      cd /home/olivia/services
+      if [ -d oliviabot ]; then
+        cd oliviabot
+        git pull --rebase
+      else
+        git clone git@github.com:RocketRace/oliviabot.git
+      fi
+    '';
+    script = ''
+      cd /home/olivia/services/oliviabot
+      nix run
+    '';
   };
 }
